32ae03c0f6361871429d0cb89d2dc7e66ebea09d
Gahramanoval301 <lemangahramanoval@gmail.com>
2023-11-13 22:21:53 +0300

    star pattern in python

---
+++ programminTech/L6/starpattern.py
@@ -0,0 +1,15 @@
+# rectangle pattern
+for i in range(0,5):
+    for j in range(0,5):
+        print('*', end='')
+    print()
+# left start rectangle
+for i in range(1,6):
+    for j in (1, i+1):
+        print('*', end='')
+    print()
+
+n=0
+while n:
+    print('Men okuzem!')
+
